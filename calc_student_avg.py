47fee6d58bfbc6619cd164be5ed123d066e45df4
Brady Hammond <brady_hammond1.baylor.edu>
2022-02-04 17:51:27 -0600

    student averages

--- calc_student_avg.py
+++ calc_student_avg.py
@@ -4,8 +4,12 @@ students = open("Student_Scores.csv", "r")
 
 student_file = csv.reader(students, delimiter=",")
 
+outfile = open("student_avg.csv", "w")
+
 next(student_file)
 
 for row in student_file:
     avg = (int(row[1]) + int(row[2]) + int(row[3])) / 3
-    prin
+    studentAvg = row[0] + "," + str(float(avg))
+    outfile.write(studentAvg + "\n")
+outfile.close()
